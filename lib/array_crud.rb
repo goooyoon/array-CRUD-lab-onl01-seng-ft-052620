023f83a549a5eaa5c768fd89e05c3cb43f4bc07b
Grace Kim <goooyoon@gmail.com>
2020-05-28 00:32:40 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,11 +7,11 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  array.push("tablet")
+  array.push("array")
 end
 
 def add_element_to_start_of_array(array, element)
-  array.unshift("watch")
+  array.unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
@@ -23,10 +23,11 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  array []
+  array[0]
 end
 
 def retrieve_first_element_from_array(array)
+  index = 0
   
 end
 
