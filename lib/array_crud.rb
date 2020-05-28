563c533c0fa0f071ef389eb2ad7fc0f3e5c2d076
Grace Kim <goooyoon@gmail.com>
2020-05-28 00:16:36 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,15 +3,15 @@ def create_an_empty_array
 end
 
 def create_an_array
-  [radio, television, computer, cellphone]
+  ["radio", "television", "computer", "cellphone"]
 end
 
 def add_element_to_end_of_array(array, element)
-  array << 
+  array.push("tablet")
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  array.unshift("")
 end
 
 def remove_element_from_end_of_array(array)
